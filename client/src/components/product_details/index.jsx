541382a3d052c402bd9fb5c2aefa342367ded557
Styles <stylesk11@gmail.com>
2024-03-01 15:41:48 -0800

    add get request for style info; track in state

--- client/src/components/product_details/index.jsx
+++ client/src/components/product_details/index.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const ProductDetail = () => {
+const ProductDetail = ({ product, server, options }) => {
+  let endpoint = (`${server}/products/${product.id}/styles`);
+
+  const [allStyles, setAllStyles] = useState([]);
+  const [style, setStyle] = useState({});
+
+  useEffect(() => {
+    axios.get(endpoint, options)
+    .then((response) => {
+      console.log('response:', response.data.results);
+      setAllStyles(response.data.results);
+      setStyle(response.data.results[0]);
+    })
+  }, [product])
 
   return (
     <div>
-      test text
     </div>
   )
 }
