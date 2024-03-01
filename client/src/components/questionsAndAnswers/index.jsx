9ed67b0b6f1b9837d415f14e523595bcec80755f
Flora Zhang <Azurstyle@outlook.com>
2024-03-01 18:39:33 -0500

    render the questionlist component

--- client/src/components/questionsAndAnswers/index.jsx
+++ client/src/components/questionsAndAnswers/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
 
-const QandA = () => {
+import React  ,{ useState, useEffect } from 'react';
+import QuestionList from './QuestionList.jsx';
+import axios from 'axios';
+
+const QandA = ({product, server, options}) => {
+
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${server}/qa/questions?product_id=${product.id}`, options)
+    .then((response) => {
+      console.log(response.data);
+      setQuestions(response.data);
+    })
+    .catch((err) => console.log(err))
+  },[product])
+
 
   return (
     <div>
       <h4>questions & Answers</h4>
+      {/* <nav>Search</nav> */}
+      <QuestionList questions={questions}/>
+
     </div>
   )
 }
