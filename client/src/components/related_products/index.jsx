589c3cd558ab950227fdb881d39d2eb9f0ba9491
mmcclanahan <mmmcclan365@gmail.com>
2024-03-01 10:56:22 -0800

    starting creating useEffect

--- client/src/components/related_products/index.jsx
+++ client/src/components/related_products/index.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const RelatedProducts = () => {
+const RelatedProducts = ({product, options, server}) => {
+  const [related, setRelated] = useState([])
+  console.log(product.id, 'component props')
+  console.log(server, token)
+  //i have id i need to hit server to get related products
+  useEffect(() => {
+    axios.get(`${server}/products/:${related}`, options)
+    .then((response) => {
+
+    })
+  }, [])
+  //i need to hit server to get each product
+
   return (
     <div>
       This is my related products component!
