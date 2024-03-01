294012b9b0aba255f017d11598666857b56ee9bd
mmcclanahan <mmmcclan365@gmail.com>
2024-03-01 15:39:19 -0800

    setup state for relatedIds / created RelatedCard component with state of current productDetails

--- client/src/components/related_products/index.jsx
+++ client/src/components/related_products/index.jsx
@@ -1,24 +1,44 @@
 import React , { useState, useEffect } from 'react'
 import axios from 'axios'
+import RelatedCard from './RelatedCard.jsx'
 
 const RelatedProducts = ({product, options, server}) => {
-  const [related, setRelated] = useState([])
-  console.log(product.id, 'component props')
-  console.log(server, token)
-  //i have id i need to hit server to get related products
-  useEffect(() => {
-    axios.get(`${server}/products/:${related}`, options)
-    .then((response) => {
+  const [relatedIds, setRelatedIds] = useState([])
 
-    })
-  }, [])
-  //i need to hit server to get each product
+  useEffect(() => {
+    if (product) {
+      axios.get(`${server}/products/${product.id}/related`, options)
+      .then((response) => {
+        setRelatedIds(response.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  }, [product])
 
   return (
-    <div>
-      This is my related products component!
+    <div id='relatedProductsCards'>
+      {relatedIds.map(ID => {
+        return <RelatedCard key={ID} id={ID} server={server} options={options} />
+      })}
     </div>
   )
 }
+//function idea for running get for an array of ids
+  /*
+  const getAllRelatedProducts = (idArray) => {
+    idArray.forEach((id) => {
+      console.log(id)
+      axios.get(`${server}/products/${id}`, options)
+      .then((result)=> {
+        setRelatedProducts(relatedProducts.concat(result.data))
+      })
+      .catch((err) => {
+        console.log('function err', err)
+      })
+    })
+  }
+  */
 
 export default RelatedProducts;
