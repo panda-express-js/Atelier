07700f748ecd87801b7175cbad636b2b25d5023e
Styles <stylesk11@gmail.com>
2024-03-03 15:03:15 -0800

    update props passed down to components

--- client/src/components/product_details/index.jsx
+++ client/src/components/product_details/index.jsx
@@ -4,30 +4,12 @@ import Photos from './photos.jsx';
 import Details from './details.jsx';
 import Summary from './summary.jsx';
 
-const ProductDetail = ({ product, server, options }) => {
-
-
-  const [allStyles, setAllStyles] = useState([]);
-  const [style, setStyle] = useState({});
-
-  useEffect(() => {
-    if (product.id) {
-      let endpoint = `${server}/products/${product.id}/styles`;
-      axios.get(endpoint, options)
-      .then((response) => {
-        setAllStyles(response.data.results);
-        setStyle(response.data.results[0]);
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
-  }, [product])
+const ProductDetail = ({ product, allStyles, style, reviews, server, options }) => {
 
   return (
     <div>
       <Photos style={style} />
-      <Details style={style} />
+      <Details product={product} allStyles={allStyles} style={style} reviews={reviews} />
       <Summary product={product} />
     </div>
   )
