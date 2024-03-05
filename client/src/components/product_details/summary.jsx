ceca9843e1578bdba00171549b973513358c8707
Styles <stylesk11@gmail.com>
2024-03-05 10:55:29 -0800

    add dynamic render for slogan & description

--- client/src/components/product_details/summary.jsx
+++ client/src/components/product_details/summary.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Summary = ({ product }) => {
-  console.log('from sum:', product);
+
+  const renderFeatures = () => {
+    if (product.features) {
+      return product.features.map((feat) => {
+        return (
+          <div key={feat.value + feat.feature}>
+            <FontAwesomeIcon icon={faCheck} className="checkmark"/>
+            {feat.value} {feat.feature}
+          </div>
+        )
+      })
+    }
+  }
 
   return (
     <div>
-      <div>
+      <div className="sloganDesc">
         <div>{product.slogan}</div>
         <div>{product.description}</div>
       </div>
-      <div></div>
+      <div className="features">
+        {renderFeatures()}
+      </div>
     </div>
   )
 }
