8cedc556c2f3fc1e66c53008dd6ccd07497e6450
MwaniN <brooklynewyork@gmail.com>
2024-03-04 19:47:23 -0500

    Css working for partial stars

--- client/src/components/ratings/star_rating/Star_Display.jsx
+++ client/src/components/ratings/star_rating/Star_Display.jsx
@@ -2,12 +2,16 @@ import React , { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
+// Star Rating - Solid or outlined stars, where the solid stars represent the review score. A total of 5 stars should always appear,
+//  and the amount filled in should correspond to the average score.
+// The visual for rating should be representative of up to a quarter of a review point.
+//  ** For example, if the average is 3.8, this should display as 3¾ solid stars and 1¼ outlined stars.
+
 // takes in the number that's passed through and only handles the job of displaying the stars
    // (this way it can be used by "Average" star rating as well as individual review stars)
 
 export default function StarDisplay (props) {
   // props.rating should be the number
-
   // number goes in => stars come out
 
   // ** replace number with props.rating after AverageStarRating is implemented
@@ -42,15 +46,28 @@ export default function StarDisplay (props) {
   // with a different className based on the number in the array
 
   return (
-    <>
+    <div className="star-box">
+      <div className="star-box-empty">
     {
     function() {
       let ratingDisplay = ratingArray.map((num) => {
-          return <FontAwesomeIcon icon={faStar} className={`star-${num}`} />
+          return <FontAwesomeIcon icon={faStar} className={`star-empty`} />
       })
       return ratingDisplay;
     }()
   }
-  </>
+  <div className="star-box-full" style={{"width": "50%"}}>
+  {
+    function() {
+      let ratingDisplay = ratingArray.map((num) => {
+          return <FontAwesomeIcon icon={faStar} className={`star-full`} />
+      })
+      return ratingDisplay;
+    }()
+  }
+  </div>
+  </div>
+
+  </div>
   );
 };
