61ffea2abb32a48204a25bd955d58150752adafd
MwaniN <brooklynewyork@gmail.com>
2024-03-06 13:37:40 -0500

    Test working

--- client/src/Tests/ratings-test.spec.js
+++ client/src/Tests/ratings-test.spec.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import StarDisplay from '../ratings/star_rating/Star_Display.jsx'
-import ReviewList from '../ratings/ReviewList.jsx'
+import ReviewTile from '../components/ratings/ReviewTile.jsx'
 
-// Sample data
-
-let rating = 4;
-
-describe(Rating, () => {
-
-  it('renders the correct rating width', () => {
-    render(<ReviewList reviews={reviews} product={product} server={server} options={options} setReviews={setReviews}/>);
-    expect(screen.getByText("Insert Text")).toBeInTheDocument();
+it('renders the review data properly', () => {
+    render(<ReviewTile rating={4} date={"Jan 4th"} username={"Billy"} summary={"It's a summary"} body={"Body Body Body"}/>);
+    expect(screen.getByText("Billy")).toBeInTheDocument();
   })
-})
