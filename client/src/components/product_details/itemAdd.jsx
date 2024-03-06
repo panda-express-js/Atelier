26bba45c8e2beb20b03ac3b224b45c71c0160c1e
Styles <stylesk11@gmail.com>
2024-03-05 16:04:09 -0800

    add ability to select size/quantity of product

---
+++ client/src/components/product_details/itemAdd.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+
+const ItemAdd = ({ style }) => {
+
+  const [selectedSKU, setSelectedSKU] = useState(null);
+  const [selectedQuantity, setSelectedQuantity] = useState(null);
+
+  // size might be NULL
+
+  const renderSizes = () => {
+    if (style.skus) {
+      let allSkus = Object.keys(style.skus);
+      return allSkus.map((sku) => {
+        return (
+          <option key={sku} value={sku}>{style.skus[sku].size}</option>
+        )
+      })
+    }
+  }
+
+  const renderQuantities = () => {
+    if (style.skus) {
+      if (style.skus[selectedSKU]) {
+        let maximum = style.skus[selectedSKU].quantity;
+        if (maximum > 15) {
+          maximum = 15;
+        }
+        let quantities = [];
+        for (let i = 1; i <= maximum; i++) {
+          quantities.push(<option key={i}>{i}</option>);
+        }
+        return quantities;
+      }
+    }
+  }
+  // adding to bag
+  // favoriting
+
+  return (
+    <div>
+      <form>
+        <select className="selector" onChange={(e) => setSelectedSKU(e.target.value)}>
+          <option value="">SELECT SIZE</option>
+          {renderSizes()}
+        </select>
+        <select className="selector">
+          <option value="">-</option>
+          {renderQuantities()}
+        </select>
+        <button>
+          <span>ADD TO BAG</span>
+          <span>+</span>
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faStar} />
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default ItemAdd;
