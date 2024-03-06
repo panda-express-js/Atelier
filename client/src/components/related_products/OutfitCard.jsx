baa9cb4e2c4947ef78e60ca036d49cfca7dd7d3e
mmcclanahan <mmmcclan365@gmail.com>
2024-03-06 15:35:18 -0800

    used local storage to store products for the outfit added button to add and delete

--- client/src/components/related_products/OutfitCard.jsx
+++ client/src/components/related_products/OutfitCard.jsx
@@ -1,9 +1,21 @@
 import React, {useState, useEffect} from 'react';
 
-const OutfitCard = ({id}) => {
+const OutfitCard = ({obj, deleteOutfit}) => {
 
   return (
-    <div> {id} </div>
+    <div>
+    <button onClick={()=> {deleteOutfit(obj.id)}}>delete x icon</button>
+    <div className='card' onClick={()=>{changeId(id)}}style={{ display: 'flex', flexDirection: 'column' }}>
+      {obj.url ? <img alt={`product image of ${obj.name}`} width='50px'src={obj.url} />: <p>No Product Image</p>}
+      <span>{obj.category}</span>
+      <span>{obj.name}</span>
+        {obj.sale_price ? <>
+                        <span style={{ color:'red' }}>{obj.sale_price}</span>
+                        <span style={{ textDecoration: 'line-through' }}>{obj.default_price}</span>
+                      </>: <span>{obj.default_price}</span>}
+        <span>rating</span>
+    </div>
+  </div>
   )
 }
 
