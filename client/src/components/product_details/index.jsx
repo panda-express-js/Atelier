75c9f6e4acdc3f54993f46b18ab04ac0e70347fd
Styles <stylesk11@gmail.com>
2024-03-06 11:42:02 -0800

    pass setStyle method to details

--- client/src/components/product_details/index.jsx
+++ client/src/components/product_details/index.jsx
@@ -4,12 +4,12 @@ import Photos from './photos.jsx';
 import Details from './details.jsx';
 import Summary from './summary.jsx';
 
-const ProductDetail = ({ product, allStyles, style, reviews, server, options }) => {
+const ProductDetail = ({ product, allStyles, style, reviews, server, options, setStyle }) => {
 
   return (
     <div>
       <Photos style={style} />
-      <Details product={product} allStyles={allStyles} style={style} reviews={reviews} />
+      <Details product={product} allStyles={allStyles} style={style} reviews={reviews} setStyle={setStyle} />
       <Summary product={product} />
     </div>
   )
