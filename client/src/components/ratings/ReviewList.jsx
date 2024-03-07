9b33e28c7e8fd730a142a3be202a951566283f83
MwaniN <brooklynewyork@gmail.com>
2024-03-07 15:24:49 -0500

    Getting all reviews with brute force solution

--- client/src/components/ratings/ReviewList.jsx
+++ client/src/components/ratings/ReviewList.jsx
@@ -19,11 +19,25 @@ export default function ReviewList ({ product , server, options, reviews, setRev
 
   // call the API and update review state using useEffect
 
+  // try calling 500 items, and if the resulting array does not contain 500 items keep increasing it by 250.
+  // need to find a way to limit the amount shown on the page before I break the page by showing all at once though.
+
+  // make count dynamic
+
+
   useEffect(() => {
-    axios.get(`${server}/reviews?product_id=${product.id}&sort=${sort}&count=2`, options).then( (response) => {
+    let count = 1000000;
+
+    axios.get(`${server}/reviews?product_id=${product.id}&sort=${sort}&count=${count}`, options).then( (response) => {
+        // if (response.data.length === count){
+        // count = count + 250;
+        // setSort(sort)}
       setReviews(response.data)
+
     }).catch((err) => console.log(err))
   },[sort])
+
+
 
 
   if(reviews.results) {console.log(reviews.results, " these are the reviews passed down from generation to generation")};
@@ -41,11 +55,14 @@ export default function ReviewList ({ product , server, options, reviews, setRev
       <br></br>
     </div>
     {function () {
+      let reviewCount = 0;
       if (reviews.results){
       let currReviews = reviews.results.map((review) => {
+        reviewCount++;
         return <ReviewTile rating={review.rating} date={review.date} username={review.reviewer_name}
         summary={review.summary} body={review.body} />
       })
+      console.log(reviewCount);
       return currReviews;}
     }()}
   </div>
