eeaa3468f420ce2bf49beb70ecc74379adda53b5
MwaniN <brooklynewyork@gmail.com>
2024-03-08 17:43:29 -0500

    Human readable date format added

--- client/src/components/ratings/ReviewTile.jsx
+++ client/src/components/ratings/ReviewTile.jsx
@@ -4,10 +4,12 @@ import StarDisplay from './star_rating/Star_Display.jsx'
 
 export default function ReviewTile ({ rating, date, username, summary, body }) {
 
+  const newDate = new Date(date).toDateString();
+
 
   return <div className="review-tile">
     <div className="username">{username} </div>
-    <div className="date">{date}</div>
+    <div className="date">{newDate}</div>
     <br></br>
     <StarDisplay rating={rating} />
     <br></br>
