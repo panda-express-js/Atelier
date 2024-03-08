0fb89081096341498a93e411add39a34017e935a
MwaniN <brooklynewyork@gmail.com>
2024-03-08 14:52:42 -0500

    Ratings bar chart working and almost done

--- client/src/components/ratings/RatingBreakdown.jsx
+++ client/src/components/ratings/RatingBreakdown.jsx
@@ -1,5 +1,61 @@
 import React , { useState, useEffect } from 'react';
 import StarDisplay from './star_rating/Star_Display.jsx'
+import { Bar } from 'react-chartjs-2';
+
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+);
+
+const HorizontalBarChart = ({ratings}) => {
+
+  console.log(ratings, " this is ratings from within the Horizontal Bar Chart")
+
+  let dataArray = []
+
+  dataArray[0] = Number(ratings['1'])
+  dataArray[1] = Number(ratings['2'])
+  dataArray[2] = Number(ratings['3'])
+  dataArray[3] = Number(ratings['4'])
+  dataArray[4] = Number(ratings['5'])
+
+
+  const data = {
+    labels: ['1', '2', '3', '4', '5'],
+    datasets: [
+      {
+        label:'Ratings',
+        data: dataArray,
+        backgroundColor: '#61DBFB'
+      }
+    ]
+  }
+  const options = {
+    indexAxis:  'y',
+    layout: {
+      padding: '2rem'
+    }
+  };
+
+  return (
+    <div>
+
+      <Bar data={data} options={options}></Bar>
+    </div>
+  )
+}
 
 
 // use metadata for review averages
@@ -25,7 +81,7 @@ function averageRating (ratingObject) {
 
   somethingelse = parseFloat(somethingelse.toFixed(5))
 
-  return rating;
+  return somethingelse;
 
 }
 
@@ -43,5 +99,6 @@ export default function RatingBreakdown ({ reviewMeta, ratingFilter, setRatingFi
   return <div className="Rating Breakdown">
     <>This is the average rating</>
     <StarDisplay rating={productAvgRating} />
+    <HorizontalBarChart ratings={reviewMeta.ratings}/>
   </div>
 }
