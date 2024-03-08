98bff2b84550077d8ec54949a7fc5bc981b004b6
mmcclanahan <mmmcclan365@gmail.com>
2024-03-07 17:46:44 -0800

    added styling

--- client/src/components/related_products/RelatedCardsCarousel.jsx
+++ client/src/components/related_products/RelatedCardsCarousel.jsx
@@ -50,9 +50,9 @@ const RelatedCardsCarousel = ({uniqueProductIds, server, options, changeId, prod
   }
 
   return (
-    <div style={{ display: 'flex' }}>
+    <div className='relatedCarousel'>
     <div>{currentPosition === 0 ? null: <button onClick={() => {leftArrow()}}>{'<'}</button>}</div>
-    <div style={{ display: 'flex' }}>
+    <div className='relatedCardsDiv'>
       {productList.map((obj, index) => {
         if (index >= currentPosition && index <= currentPosition + 3) {
           return <RelatedCard key={obj.id} id={obj.id} obj={obj} changeId={changeId} product={product} style={style}/>
