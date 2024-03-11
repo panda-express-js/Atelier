4955b0bdb8ddbe216a82b2cda919a60f6d73bf56
mmcclanahan <mmmcclan365@gmail.com>
2024-03-11 15:11:35 -0700

    added FE functionality need to style

--- client/src/components/related_products/OutfitCard.jsx
+++ client/src/components/related_products/OutfitCard.jsx
@@ -2,14 +2,19 @@ import React, {useState, useEffect} from 'react';
 import StarDisplay from '../ratings/star_rating/Star_Display.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCircleXmark} from '@fortawesome/free-solid-svg-icons'
-
+import PhotoView from './PhotoView.jsx'
 const OutfitCard = ({obj, deleteOutfit, changeId}) => {
-
+//{obj.url ? <img onClick={()=>{changeId(obj.id)}}alt={`product image of ${obj.name}`} width='50px'src={obj.url} />: <img onClick={()=>{changeId(obj.id)}}/>}
+const [photo, setPhoto] = useState(obj.photosArray[0].url);
+const changePhoto = (url) => {
+  setPhoto(url);
+}
   return (
     <div className='outfitCardDiv'>
       <div className='cardTop'>
         <FontAwesomeIcon className='BTN' icon={faCircleXmark} onClick={()=> {deleteOutfit(obj.id)}}/>
-        {obj.url ? <img onClick={()=>{changeId(obj.id)}}alt={`product image of ${obj.name}`} width='50px'src={obj.url} />: <img onClick={()=>{changeId(obj.id)}}/>}
+        <img className='mainImg' onClick={()=>{changeId(obj.id)}}alt={`product image of ${obj.name}`} width='50px'src={photo} />
+      <PhotoView photos={obj.photosArray} photo={photo} changePhoto={changePhoto}/>
       </div>
       <div className="cardBottom"  onClick={()=>{changeId(obj.id)}}>
         <span className='cardCategory'>{obj.category}</span>
