5a51b6740d6b37e081b1297ea3db4c1edd508b32
mmcclanahan <mmmcclan365@gmail.com>
2024-03-10 23:57:34 -0700

    gotta put back in other tests

--- client/src/Tests/ratings-test.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-
-import ReviewTile from '../components/ratings/ReviewTile.jsx'
-
-it('renders the review data properly', () => {
-    render(<ReviewTile rating={4} date={"Jan 4th"} username={"Billy"} summary={"It's a summary"} body={"Body Body Body"}/>);
-    expect(screen.getByText("Billy")).toBeInTheDocument();
-  })
