b8a24c38964f25da2910af113ceabf8a60903f5f
mmcclanahan <mmmcclan365@gmail.com>
2024-03-12 23:19:51 -0700

    100% index file, added id in index.js , added new file to avoid overwriting tests

--- client/src/components/ratings/Index.jsx
+++ client/src/components/ratings/Index.jsx
@@ -5,7 +5,7 @@ import ReviewList from './ReviewList.jsx'
 export default function RatingsReviews ({ server, options, product, reviews, setReviews, avgRating }) {
 
   return (
-  <div id="Ratings & Reviews">
+  <div id="Ratings & Reviews" data-testid='R&R'>
     <h2>Ratings & Reviews</h2>
     <ReviewList reviews={reviews} product={product} server={server} options={options} setReviews={setReviews} avgRating={avgRating}/>
     <br></br>
